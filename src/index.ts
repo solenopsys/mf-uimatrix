488c3a978e34a6b99cecba7185f55b76007c0e0b
AlexStorm <info@alexstorm.ru>
2023-08-02 12:53:22 +0530

    entry refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import {AppModule} from "./lib/app.module";
+
 export * from './lib/app.module';
+
+import {XsModule, XsModuleType} from "@solenopsys/fl-globals";
+
+
+
+export const ENTRY: XsModule<AppModule> = {
+    module: AppModule,
+    type: XsModuleType.PLATFORM,
+};
